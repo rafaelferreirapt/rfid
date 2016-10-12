bb3291264836e68eb5a46fc82621a3bcefdeb045
du8 <dod_08_@hotmail.com>
2016-10-12 15:28:23 +0100

    spinner e imagem

--- android/app/src/main/java/com/example/dod_0/rfid/reader.java
+++ android/app/src/main/java/com/example/dod_0/rfid/reader.java
@@ -1,10 +1,17 @@
 package com.example.dod_0.rfid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
-
+import java.io.InputStream;
 
 
 public class reader extends AppCompatActivity {
@@ -21,5 +28,45 @@ public class reader extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+
+        String abc = "http://www.hipersuper.pt/wp-content/uploads/2014/12/image005.jpg";
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView2)).execute(abc);
+
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+    public void onNothingSelected(AdapterView parent) {
+        // Do nothing.
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
+
