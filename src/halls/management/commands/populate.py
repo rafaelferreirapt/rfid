54df39552e189a3fc45041d518622d4d42e89c84
Rafael Ferreira <mail@rafaelferreira.pt>
2016-10-13 16:18:18 +0100

    videos

--- src/halls/management/commands/populate.py
+++ src/halls/management/commands/populate.py
@@ -19,22 +19,22 @@ class Command(BaseCommand):
         content1_1 = ContentHall.objects.create(hall=hall1, media="image", url="https://www.continente.pt/stores/continente/PublishingImages/Images/PageView/assinatura/imagem-porco.jpg")
         content1_2 = ContentHall.objects.create(hall=hall1, media="image", url="https://campanha.continente.pt/images/971x389.jpg")
         content1_3 = ContentHall.objects.create(hall=hall1, media="image", url="http://c3.quickcachr.fotos.sapo.pt/i/o1213706d/15171764_cG9c8.jpeg")
-        content1_4 = ContentHall.objects.create(hall=hall1, media="video", url="https://www.youtube.com/watch?v=cn0cyshPEgE")
+        content1_4 = ContentHall.objects.create(hall=hall1, media="video", url="/static/video1.mp4")
 
         content2_1 = ContentHall.objects.create(hall=hall2, media="image", url="https://static.noticiasaominuto.com/stockimages/1370x587/naom_51424ad93b6bd.jpg")
         content2_2 = ContentHall.objects.create(hall=hall2, media="image", url="http://www.promocoesedescontos.com/wp-content/uploads/2016/07/Captura-de-ecra%CC%83-2016-07-25-a%CC%80s-21.32.15.png")
         content2_3 = ContentHall.objects.create(hall=hall2, media="image", url="https://i.ytimg.com/vi/rC4YuHlTEUM/maxresdefault.jpg")
-        content2_4 = ContentHall.objects.create(hall=hall2, media="video", url="https://www.youtube.com/watch?v=aADwoFu2fWs")
+        content2_4 = ContentHall.objects.create(hall=hall2, media="video", url="/static/video2.mp4")
 
         content3_1 = ContentHall.objects.create(hall=hall3, media="image", url="http://6.fotos.web.sapo.io/i/o8d148530/18278804_4Tn31.jpeg")
         content3_2 = ContentHall.objects.create(hall=hall3, media="image", url="http://5.fotos.web.sapo.io/i/G361150e3/17830920_L9zAc.jpeg")
         content3_3 = ContentHall.objects.create(hall=hall3, media="image", url="http://6.fotos.web.sapo.io/i/G2c11967d/17966185_hCtKw.jpeg")
-        content3_4 = ContentHall.objects.create(hall=hall3, media="video", url="https://www.youtube.com/watch?v=du1_Mn9a8IU")
+        content3_4 = ContentHall.objects.create(hall=hall3, media="video", url="h/static/video3.mp4")
 
         content4_1 = ContentHall.objects.create(hall=hall6, media="image", url="http://www.promocoesedescontos.com/wp-content/uploads/2016/07/Captura-de-ecra%CC%83-2016-06-27-a%CC%80s-21.44.56.png")
         content4_2 = ContentHall.objects.create(hall=hall6, media="image", url="http://globodicas.com.br/wp-content/uploads/2011/11/promocoes.jpg")
         content4_3 = ContentHall.objects.create(hall=hall6, media="image", url="http://thumbs.web.sapo.io/?Q=70&H=1610&W=1899&epic=gYnYmiSzhM+iVZRYVwVtQnQPKL0AAGLF+KxkqhpDZg4IDsqJB769LSb3qQXJRd6cfVBLxeo5dLYIB573BOasaqYdEUkTy+qoF0kyYzNBMXnmXhQ=")
-        content4_4 = ContentHall.objects.create(hall=hall6, media="video", url="https://www.youtube.com/watch?v=ctAQwBK_Vzw")
+        content4_4 = ContentHall.objects.create(hall=hall6, media="video", url="/static/video4.mp4")
 
         category1 = Category.objects.create(name="Massas", description="Massas para cozinha.")
         category2 = Category.objects.create(name="Águas", description="Águas.")
